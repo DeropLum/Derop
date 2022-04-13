410be62be11fff0dc0e3a827477919a4184eb050
DeropLum <kaa0124@mail.ru>
2022-04-13 20:56:16 +0300

    Find roots of the quadratic equation

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
-sides = sorted(sides, reverse=True)
-smax = 0
-for i in range(len(sides)):
-  for j in range(i + 1, len(sides)):
-    for k in range(j + 1, len(sides)):
-      a = sides[i]
-      b = sides[j]
-      c = sides[k]
-      if a + b > c and a + c > b and b + c > a:
-         p = (a + b + c) / 2
-         s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
-         if s > smax:
-            smax = s
-print("Максимальная площадь треугольника", smax)
+from math import sqrt
+print("a = ")
+a = int(input())
+print("b = ")
+b = int(input())
+print("c = ")
+c = int(input())
+
+d = b*b - 4*a*c
+if d >= 0:
+    x1 = (-b + sqrt(d)) / (2*a)
+    x2 = (-b - sqrt(d)) / (2*a)
+    answ = "x1 = %.3f x2 = %.3f" % (x1, x2)
+else:
+    answ = "Дискриминант = %s. Нет решения" % d
+    print(answ)
